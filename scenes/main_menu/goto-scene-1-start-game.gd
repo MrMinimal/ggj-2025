0b5d87d53640ea5efabc1063225a689d12c0afaf
Tom Langwaldt <t.langwaldt@gmail.com>
2025-01-25 16:23:16 +0100

    Fix scene switching

--- scenes/main_menu/goto-scene-1-start-game.gd
+++ scenes/main_menu/goto-scene-1-start-game.gd
@@ -1,4 +1,4 @@
 extends MarginContainer
 
 func _on_new_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level_0/level_0.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
