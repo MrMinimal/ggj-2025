fc1293d208b8845322df48b04bece603c8a183dc
Elias Steurer <i@kelteseth.net>
2025-01-25 17:36:32 +0100

    Add plastic bag attachment to player

--- src/enemies/plastic_bag.gd
+++ src/enemies/plastic_bag.gd
@@ -1,10 +1,12 @@
 extends Node3D
+class_name PlasticBag
 
 func _ready():
 	$AnimationPlayer.play("plastic_bag_fly")
 	
-	
-func _on_area_3d_body_entered(body):
-	if body.is_in_group("player"):
-		pass
-	pass # Replace with function body.
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if not body.is_in_group("player"):
+		return
+	var player = body as Player
+	player.apply_debuff_plastic_bag(self)
